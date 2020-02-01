28ea4424eee41524c1600df57184854ec43606c5
JP Martin <jpmartin@example.com>
2020-02-01 14:34:24 -0800

    Optional debug text canvas (deactivate to hide)

--- Assets/Scripts/SnapZoneAxialManager.cs
+++ Assets/Scripts/SnapZoneAxialManager.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SnapZoneAxialManager : MonoBehaviour
 {
     public Transform touchingObject;
     public PartAxelOut rot1, rot2, rot3, rot4;
+    private GameObject debugText = null;
 
+    public void Start()
+    {
+        debugText = GameObject.Find("DebugText");
+        if (debugText==null) {
+            Debug.Log("Debug text canvas not found, continuing.");
+        }
+    }
     void Update()
     {
         if(touchingObject != null)
@@ -24,6 +33,10 @@ public class SnapZoneAxialManager : MonoBehaviour
             }
             int chooseRot = Mathf.FloorToInt(angle / 90);
             Debug.Log("Rotation: " + chooseRot);
+            if (null!=debugText) {
+                Text foo = debugText.GetComponent("Text") as Text;
+                foo.text = "Rot: " + chooseRot;
+            }
             rot1.gameObject.SetActive(chooseRot == 0);
             rot2.gameObject.SetActive(chooseRot == 1);
             rot3.gameObject.SetActive(chooseRot == 2);
