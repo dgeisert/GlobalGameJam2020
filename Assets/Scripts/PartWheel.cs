ab54ee8b2d473c71d5a9ef4985c830b392c7deed
dgeisert <memphis.gatech@gmail.com>
2020-02-02 14:28:40 -0800

    Level 6

--- Assets/Scripts/PartWheel.cs
+++ Assets/Scripts/PartWheel.cs
@@ -11,7 +11,7 @@ public class PartWheel : PartBase
         {
             wheel.Rotate(new Vector3(0, (leftSide ? -1 : 1) * control * Time.deltaTime * 10, 0));
             PartCore.Instance.rb.AddForceAtPosition(-PartCore.Instance.transform.forward * control / 10 * Time.deltaTime,
-            PartCore.Instance.transform.position - PartCore.Instance.transform.up * 0.2f, ForceMode.VelocityChange);
+            PartCore.Instance.transform.position - Vector3.up * 0.2f, ForceMode.VelocityChange);
             PartCore.Instance.transform.Rotate(0, (leftSide ? 1 : -1) * control / 6 * Time.deltaTime * 10, 0);
         }
     }
