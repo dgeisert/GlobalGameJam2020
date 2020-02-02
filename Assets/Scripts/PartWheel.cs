dd087ebdc0443200c6cf7e6cfec6b7816ebd714a
dgeisert <memphis.gatech@gmail.com>
2020-02-02 12:14:50 -0800

    Wheels behaving better

--- Assets/Scripts/PartWheel.cs
+++ Assets/Scripts/PartWheel.cs
@@ -5,27 +5,13 @@ using UnityEngine;
 public class PartWheel : PartBase
 {
     public Transform wheel;
-    public bool grounded = false;
     public override void PartUpdate()
     {
-        wheel.Rotate(new Vector3(0, (leftSide ? -1 : 1) * control * Time.deltaTime * 10, 0));
-        PartCore.Instance.rb.AddForce(-PartCore.Instance.transform.forward * control / 10 * Time.deltaTime, ForceMode.VelocityChange);
-        PartCore.Instance.transform.Rotate(0, (leftSide ? 1 : -1) * control / 2 * Time.deltaTime * 10 * Vector3.Distance(transform.position, PartCore.Instance.transform.position), 0);
-    }
-
-    public void OnCollisionEnter(Collision collision)
-    {
-        if(collision.collider.tag == "Ground")
+        if(transform.position.y < 0.2f)
         {
-            grounded = true;
-        }
-    }
-
-    public void OnCollisionLeave(Collision collision)
-    {
-        if(collision.collider.tag == "Ground")
-        {
-            grounded = false;
+            wheel.Rotate(new Vector3(0, (leftSide ? -1 : 1) * control * Time.deltaTime * 10, 0));
+            PartCore.Instance.rb.AddForce(-PartCore.Instance.transform.forward * control / 10 * Time.deltaTime, ForceMode.VelocityChange);
+            PartCore.Instance.transform.Rotate(0, (leftSide ? 1 : -1) * control / 2 * Time.deltaTime * 10 * Vector3.Distance(transform.position, PartCore.Instance.transform.position), 0);
         }
     }
 }
