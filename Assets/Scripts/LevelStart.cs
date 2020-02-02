a167c80cc7e6467a2860660de27da5d5527f83d5
JP Martin <jpmartin@example.com>
2020-02-02 14:56:20 -0800

    Attempt at level select

---
+++ Assets/Scripts/LevelStart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelStart : MonoBehaviour
+{
+
+    public string levelName;
+    private Text debugText = null;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject debugObj = GameObject.Find("DebugText");
+        if (null!=debugObj) {
+            debugText = debugObj.GetComponent<Text>();
+        }
+    }
+
+    public void DoIt() {
+        Log("(DoIt) Loading " + levelName);
+    }
+
+    private void Log(string logMessage)
+    {
+        Debug.Log(logMessage);
+        if (null!=debugText) {
+            Text foo = debugText.GetComponent("Text") as Text;
+            foo.text = logMessage;
+        }
+    }
+}
--- Assets/Scripts/SwitchControls.cs
+++ Assets/Scripts/SwitchControls.cs
@@ -11,14 +11,29 @@ public class SwitchControls : MonoBehaviour
     public bool movementMode {
         get { return movement.activeInHierarchy; }
         set {
-            movement.SetActive(value);
-            controller.SetActive(!value);
-            leftRemoteControl.SetActive(!value);
+            if (null!=movement) movement.SetActive(value);
+            if (null != controller) controller.SetActive(!value);
+            if (null != leftRemoteControl) leftRemoteControl.SetActive(!value);
         }
     }
 
     void Start()
     {
+        if (movement==null) {
+            Debug.Log("variable movement of SwitchControls has not been assigned. You probably need to assign the movement variable of the SwitchControls script in the inspector.");
+            movementMode = true;
+            return;
+        }
+        if (controller==null) {
+            Debug.Log("variable controller of SwitchControls has not been assigned. You probably need to assign the controller variable of the SwitchControls script in the inspector.");
+            movementMode = true;
+            return;
+        }
+        if (leftRemoteControl==null) {
+            Debug.Log("variable leftRemoteControl of SwitchControls has not been assigned. You probably need to assign the leftRemoteControl variable of the SwitchControls script in the inspector.");
+            movementMode = true;
+            return;
+        }
         // Enforce consistent visibility for all objects.
         movementMode = movementMode;
     }
