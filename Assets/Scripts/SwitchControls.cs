f97466d1435fc85f45ac10f692324ae5ce1c3ec5
dgeisert <memphis.gatech@gmail.com>
2020-02-01 22:23:11 -0800

    Movement and controls switching with B

---
+++ Assets/Scripts/SwitchControls.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchControls : MonoBehaviour
+{
+    public GameObject movement;
+    public GameObject controller;
+
+    public void DoSwitchControls()
+    {
+        movement.SetActive(!movement.activeInHierarchy);
+        controller.SetActive(!controller.activeInHierarchy);
+    }
+}
