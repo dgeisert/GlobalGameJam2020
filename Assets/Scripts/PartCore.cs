3e0ba23460e5635e1e9d2c57bb7794a29ca9c4e5
dgeisert <memphis.gatech@gmail.com>
2020-02-01 18:04:43 -0800

    Physics colliders duplication for rigidbody interactions

--- Assets/Scripts/PartCore.cs
+++ Assets/Scripts/PartCore.cs
@@ -9,6 +9,9 @@ public class PartCore : MonoBehaviour
     public Transform axelRightFront, axelRightBack, axelLeftFront, axelLeftBack;
     public List<PartAxelOut> leftAxels;
     public List<PartAxelOut> rightAxels;
+    public Transform colliderHolder;
+    public Transform colliderBase;
+    public Dictionary<PartBase, Transform> parts = new Dictionary<PartBase, Transform>();
     public float speed;
     bool left, right;
     void Start()
@@ -65,5 +68,47 @@ public class PartCore : MonoBehaviour
                 pao.control = 0;
             }
         }
+    }
+
+    void BuildColliders(PartBase pb)
+    {
+        if(pb.rb != null && !parts.ContainsKey(pb))
+        {
+            parts.Add(pb, GameObject.Instantiate(pb.primaryCollider.transform, pb.primaryCollider.transform.position, pb.primaryCollider.transform.rotation, colliderHolder));
+            parts[pb].gameObject.layer = 11;
+        }
+        foreach(PartBase partBase in pb.children)
+        {
+            BuildColliders(partBase);
+        }
+    }
+
+    public void Build()
+    {
+        StartCoroutine(DoBuild());
+    }
+
+
+    IEnumerator DoBuild()
+    {
+        yield return null;
+        foreach(PartAxelOut pao in leftAxels)
+        {
+            BuildColliders(pao);
+        }
+        foreach(PartAxelOut pao in rightAxels)
+        {
+            BuildColliders(pao);
+        }
+    }
+
+    public void Rebuild()
+    {
+        for(int i = colliderHolder.childCount - 1; i >= 0; i--)
+        {
+            Destroy(colliderHolder.GetChild(i).gameObject);
+        }
+        parts = new Dictionary<PartBase, Transform>();
+        Build();
     }
 }
